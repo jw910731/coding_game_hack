802d4194c32923c595c13d1bb8ab1f8eec3c5853
jw910731 <jw910731@gmail.com>
2022-04-22 01:27:54 +0800

    Add make tart and croisant

--- code/main.cpp
+++ code/main.cpp
@@ -8,11 +8,30 @@ int main()
         if (maker.chefState == chef::state::ALLDONE) {
             maker.recipeStratege("MaxAward");
             maker.checkLackList();
+            for (auto &it : maker.lackList) {
+                if (it == "CHOPPED_STRAWBERRIES")
+                    maker.chopList.emplace_back("STRAWBERRIES");
+                if (it == "CROISSANT") {
+                    maker.ovenList.emplace_back("DOUGH");
+                }
+                if (it == "TART") {
+                    maker.ovenList.emplace_back("RAW_TART");
+                }
+            }
         }
-        if (maker.foodDelivery())
+        if (maker.useOvenFlag) {
+            maker.take("OVEN");
+        } else if (maker.foodDelivery()) {
             continue;
-        else
+        } else if (!maker.mergeList.empty()) {
+            maker.merge();
+        } else if (!maker.chopList.empty()) {
+            maker.chop();
+        } else if (!maker.ovenList.empty()) {
+            maker.oven();
+        } else {
             maker.action("WAIT");
+        }
     }
     return 0;
 }
